7e0cb49201084ff08ac73d137f9d5b68878d74fe
Eric.W <75426841+callmeeric5@users.noreply.github.com>
2024-05-26 23:01:38 +0200

    Add files via upload

--- Deep_Learning/assignment/practical_work_2/project/model.py
+++ Deep_Learning/assignment/practical_work_2/project/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from einops.layers.torch import Rearrange
 
 
 class Net(nn.Module):
@@ -14,7 +15,7 @@ class Net(nn.Module):
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
-            nn.ReLU()
+            nn.ReLU(),
         )
 
         self.block2 = nn.Sequential(
@@ -23,17 +24,20 @@ class Net(nn.Module):
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
-            nn.ReLU()
+            nn.ReLU(),
         )
 
         self.block3 = nn.Sequential(
-            nn.Conv2d(64, 64* (upscale_factor ** 2), kernel_size=3, padding=1),
-            nn.PixelShuffle(upscale_factor),
+            nn.Conv2d(64, 64 * (upscale_factor**2), kernel_size=3, padding=1),
+            # nn.PixelShuffle(upscale_factor),
+            Rearrange(
+                "b (c uf1 uf2) h w -> b c (h uf1) (w uf2)",
+                uf1=upscale_factor,
+                uf2=upscale_factor,
+            ),  # Use einops instead of PixelShuffle
             nn.ReLU(),
-            nn.Conv2d(64, 1, kernel_size=3, padding=1) 
+            nn.Conv2d(64, 1, kernel_size=3, padding=1),
         )
-
-
 
     def forward(self, x):
         # RUN YOUR MODEL ARCHITECTURE HERE
